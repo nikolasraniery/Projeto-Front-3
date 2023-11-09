a54f86f23c43839cdc56ad12856a20894ba29d01
nikolasraniery <nikolassraniery@gmail.com>
2023-11-09 15:43:20 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const cartButton = document.querySelector("#cart-button");
+const cartContainer = document.querySelector("#cart-container");
+
+cartButton.addEventListener("click", () => {
+  cartContainer.classList.toggle("open");
+});
+
+function closeCart() {
+  cartContainer.classList.remove("open");
+}
